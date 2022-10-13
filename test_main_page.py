1a9102eef3e204fec47478a656a3fb527b58b0f7
vdv186 <82193531+VDV186@users.noreply.github.com>
2022-10-13 21:51:56 +0400

    Initialize LoginPage in test_guest_can_go_to_login_page

--- test_main_page.py
+++ test_main_page.py
@@ -7,6 +7,8 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()  # открываем страницу
     page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
+    login_page = LoginPage(browser, browser.current_url)  # Инициализируем LoginPage в теле теста
+    login_page.should_be_login_page() # проверяем наличие форм и слова логин в линке
 
 
 def test_guest_should_see_login_link(browser):
@@ -15,13 +17,5 @@ def test_guest_should_see_login_link(browser):
     page.open()
     page.should_be_login_link()
 
-
-def test_guest_should_see_forms(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_login_url()
-    page.should_be_login_form()
-    page.should_be_register_form()
 
 # pytest -v --tb=line --language=en test_main_page.py
